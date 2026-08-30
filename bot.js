d74c7e2404e2b7af132dc046b4ffc6cbb796f97d
Alisq33 <anaaa300@outlook.com>
2026-08-30 19:53:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const os = require('os');
 // ===== بيانات الحسابات =====
 const TOKEN_HOST = "576a2902-db16-4e9f-b503-3da6ba4bf78a";
 const USER_ID_HOST = 80055399;
-const TOKEN_GUEST = "6c278a87-a015-4bbc-b963-6e7196e2c652";
+const TOKEN_GUEST = "6d3ce792-7fce-40cd-aa0f-0ab34aca6354";
 const USER_ID_GUEST = 51660277;
 const GROUP_ID = 18432094;
 const WAIT_TIME = 90; // 90 ثانية (دقيقة ونصف)
